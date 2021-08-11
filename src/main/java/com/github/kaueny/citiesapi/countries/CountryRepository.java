90be060504809fa62a2f1acfea32c8920733a694
Kaueny de Oliveira Alves <kaueny.alves@raizen.com>
2021-08-10 22:03:23 -0300

    primeiro comit

---
+++ src/main/java/com/github/kaueny/citiesapi/countries/CountryRepository.java
@@ -0,0 +1,10 @@
+package com.github.kaueny.citiesapi.countries;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface CountryRepository  extends JpaRepository<Country, Long> {
+
+
+}
